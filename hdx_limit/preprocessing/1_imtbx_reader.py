e9d0396c15fa602d3ed700d1423fca2f151930fa
Allan Ferrari <ajferrari@gmail.com>
2022-03-09 17:27:43 -0600

    v0.22.068 Fix indentation level.

--- hdx_limit/preprocessing/1_imtbx_reader.py
+++ hdx_limit/preprocessing/1_imtbx_reader.py
@@ -736,7 +736,7 @@ def main(isotopes_path,
                 polyfit_coeffs=calib_dict_protein_polyfit["polyfit_coeffs"], mz=df["mz_mono_fix"])
             testq["mz_mono_fix_round"] = np.round(testq["mz_mono_fix"].values, 3)
     elif protein_polyfit and protein_calibration_outpath is not None:
-            calib_dict_protein_polyfit = gen_mz_error_calib_output(
+        calib_dict_protein_polyfit = gen_mz_error_calib_output(
                 testq=testq,
                 allseq=allseq,
                 calib_pk_fpath=calibration_outpath,
@@ -744,9 +744,9 @@ def main(isotopes_path,
                 ppm_tol=ppm_tolerance,
                 int_tol=intensity_tolerance,
                 cluster_corr_tol=cluster_corr_tolerance)
-            testq["mz_mono_fix"] = apply_polyfit_cal_mz(
+        testq["mz_mono_fix"] = apply_polyfit_cal_mz(
                 polyfit_coeffs=calib_dict_protein_polyfit["polyfit_coeffs"], mz=df["mz_mono"])
-            testq["mz_mono_fix_round"] = np.round(testq["mz_mono_fix"].values, 3)
+        testq["mz_mono_fix_round"] = np.round(testq["mz_mono_fix"].values, 3)
     else:
         # This is what is initially implemented for mz correction.
         # Identify major peak of abs_ppm_error clusters, apply correction to all monoisotopic mz values.
