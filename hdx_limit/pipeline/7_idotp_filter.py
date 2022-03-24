6fcefb4855a3143e3644b2c3e87616e1aaddfb43
Allan Ferrari <ajferrari@gmail.com>
2022-03-24 00:41:51 -0500

    v0.22.083 Generate dataframe with all ics. Compute weighted average RT and DT. Generate scatter plots saved in results/plots/tensor-recenter showing distribution of RTs/DTs and where new center lives. Checked library info contains RT_weighted_avg, DT_weighted_avg_bins and name_recentered. This values should be used to extract tensors again in the next part of the protocol.

--- hdx_limit/pipeline/7_idotp_filter.py
+++ hdx_limit/pipeline/7_idotp_filter.py
@@ -37,12 +37,135 @@ import argparse
 import pandas as pd
 import seaborn as sns
 import matplotlib as mpl
-mpl.use("Agg")
+import numpy as np
+import yaml
+import os
 import matplotlib.pyplot as plt
+from hdx_limit.core.io import limit_read
+mpl.use("Agg")
 
 
-def main(library_info_path,
+def generate_dataframe_ics(configfile,
+                           all_ics_inputs,
+                           idotp_cutoff=0.99):
+
+    # Create dictionary containing all ics passing idotp_cutoff
+    protein_ics = {}
+    for f in all_ics_inputs:
+        if f.split('/')[-1] not in protein_ics:
+            ics = [ic for ic in limit_read(f) if ic.undeut_ground_dot_product >= idotp_cutoff]
+            if len(ics) > 0:
+                protein_ics[f.split('/')[-1]] = [ics]
+        else:
+            ics = [ic for ic in limit_read(f) if ic.undeut_ground_dot_product >= idotp_cutoff]
+            if len(ics) > 0:
+                protein_ics[f.split('/')[-1]].append(ics)
+
+    # Flat list of lists of ics (all charge states will be one single list
+    for key in protein_ics:
+        protein_ics[key] = [i for sublist in protein_ics[key] for i in sublist]
+
+    # Extract values for dt, rt, auc, charge and file index from each IC and store in a dataframe
+    data = []
+    for key in protein_ics:
+        for ic in protein_ics[key]:
+            rt = ic.retention_labels[0] + (ic.retention_labels[1] - ic.retention_labels[0]) * ic.rt_com
+            dt = ic.drift_labels[0] + (ic.drift_labels[1] - ic.drift_labels[0]) * ic.dt_coms
+            auc = ic.ic_auc_with_gauss_extrapol
+            charge = ic.charge_states[0]
+            file_index = config_dict[0].index([i for i in configfile[0] if '_'.join(
+                ic.info_tuple[0].split('/')[-1].split('.')[-5:-4][0].split('_')[-4:]) in i][0])
+            idotp = ic.undeut_ground_dot_product
+
+            data.append([key, ic, rt, dt, auc, charge, file_index, idotp])
+
+    df = pd.DataFrame(data, columns=['name', 'ic', 'rt', 'dt', 'auc', 'charge', 'file_index', 'idotp'])
+    df['auc_log'] = 2 * np.log10(df['auc'])
+
+    # Find DT weighted average
+    for name, charge in set([(n, c) for (n, c) in df[['name', 'charge']].values]):
+        df.loc[(df['name'] == name) & (df['charge'] == charge), 'DT_weighted_avg'] = sum(
+            df[(df['name'] == name) & (df['charge'] == charge)]['dt'] *
+            df[(df['name'] == name) & (df['charge'] == charge)]['auc']) / sum(
+            df[(df['name'] == name) & (df['charge'] == charge)]['auc'])
+    # Find RT weighted average
+    for name in set(df['name'].values):
+        df.loc[df['name'] == name, 'RT_weighted_avg'] = sum(
+            df[df['name'] == name]['rt'] * df[df['name'] == name]['auc']) / sum(df[df['name'] == name]['auc'])
+
+    # Compute DT weighted avg in bin dimension (this value should be used to extract tensors for consistency with
+    # 5_extract_timepoint_tensor code
+    df['DT_weighted_avg_bins'] = df['DT_weighted_avg'] * 200.0 / 13.781163434903
+
+    # Plot scatter plot for each protein
+    # Create folder to save pdf files
+    if not os.path.isdir('results/plots/tensor-recenter/'):
+        os.makedirs('results/plots/tensor-recenter/')
+
+    for name in list(set(df['name'].values)):
+
+        n_charges = len(set(df[df['name'] == name]['charge'].values))
+
+        fig, ax = plt.subplots(1, n_charges + 1, dpi=200, figsize=(3 * (n_charges + 1), 2.5))
+
+        sns.scatterplot(data=df[df['name'] == name], x='dt', y='rt', palette='bright', hue='charge', size='auc_log',
+                        ax=ax[0])
+        ax[0].set_ylim(df[df['name'] == name]['RT_weighted_avg'].mean() - 0.4,
+                       df[df['name'] == name]['RT_weighted_avg'].mean() + 0.4)
+        ax[0].set_xlim(df[df['name'] == name]['DT_weighted_avg'].min() - 0.5,
+                       df[df['name'] == name]['DT_weighted_avg'].max() + 0.5)
+        ax[0].axhline(df[df['name'] == name]['RT_weighted_avg'].mean(), color='black', alpha=0.5, lw=0.5)
+        ax[0].text(0, 1.01, '%s' % name, transform=ax[0].transAxes, fontsize=8)
+        ax[0].set_xlabel('DT')
+        ax[0].set_ylabel('RT')
+
+        h, l = ax[0].get_legend_handles_labels()
+        ax[0].legend(h[1:n_charges + 1], l[1:n_charges + 1], fontsize=10, loc=2,
+                     bbox_to_anchor=(0.0, 0.15), bbox_transform=ax[0].transAxes, borderpad=0.02,
+                     columnspacing=0.,
+                     handletextpad=0.0, frameon=False, ncol=n_charges, prop={'size': 6})
+
+        for j, charge in enumerate(sorted(list(set(df[df['name'] == name]['charge'].values)))):
+            sns.scatterplot(data=df[(df['name'] == name) & (df['charge'] == charge)], x='dt', y='rt', palette='bright',
+                            hue='file_index', size='auc_log', ax=ax[j + 1])
+            ax[j + 1].text(0, 1.01, 'charge=%i+' % charge, transform=ax[j + 1].transAxes, fontsize=8)
+            ax[j + 1].set_ylim(df[df['name'] == name]['RT_weighted_avg'].mean() - 0.4,
+                               df[df['name'] == name]['RT_weighted_avg'].mean() + 0.4)
+            ax[j + 1].set_xlim(df[(df['name'] == name) & (df['charge'] == charge)]['DT_weighted_avg'].mean() * 0.94,
+                               df[(df['name'] == name) & (df['charge'] == charge)]['DT_weighted_avg'].mean() * 1.06)
+            ax[j + 1].axhline(df[df['name'] == name]['RT_weighted_avg'].mean(), color='black', alpha=0.5, lw=0.5)
+
+            ax[0].scatter(df[(df['name'] == name) & (df['charge'] == charge)]['DT_weighted_avg'].mean(),
+                          (sum(df[(df['name'] == name) & (df['charge'] == charge)]['rt'] *
+                               df[(df['name'] == name) & (df['charge'] == charge)]['auc'])) /
+                          sum(df[(df['name'] == name) & (df['charge'] == charge)]['auc']), marker='x', color='black',
+                          s=20)
+            ax[j + 1].scatter(df[(df['name'] == name) & (df['charge'] == charge)]['DT_weighted_avg'].mean(),
+                              (sum(df[(df['name'] == name) & (df['charge'] == charge)]['rt'] *
+                                   df[(df['name'] == name) & (df['charge'] == charge)]['auc'])) /
+                              sum(df[(df['name'] == name) & (df['charge'] == charge)]['auc']), marker='x',
+                              color='black', s=20)
+
+            n_files = len(set(df[(df['name'] == name) & (df['charge'] == charge)]['file_index'].values))
+            h, l = ax[j + 1].get_legend_handles_labels()
+            ax[j + 1].legend(h[1:n_files + 1], l[1:n_files + 1], fontsize=10, loc=2,
+                             bbox_to_anchor=(0.0, 0.15), bbox_transform=ax[j + 1].transAxes, borderpad=0.02,
+                             columnspacing=0.,
+                             handletextpad=0.0, frameon=False, ncol=n_files, prop={'size': 6})
+            ax[j + 1].set_xlabel('DT')
+            ax[j + 1].set_ylabel('RT')
+
+        plt.tight_layout()
+        plt.savefig('results/plots/tensor-recenter/' + name + '.pdf', format='pdf', dpi=200)
+        plt.close('all')
+
+    return df
+
+
+def main(configfile,
+         library_info_path,
          all_idotp_inputs,
+         all_ics_inputs,
          library_info_out_path=None,
          plot_out_path=None,
          return_flag=False,
@@ -50,8 +173,10 @@ def main(library_info_path,
     """Reads all library_info index idotp_check.csv files and returns or saves a list of indices with idotp >= idotp_cutoff.
 
     Args:
+        configfile (dict): configfile dict
         library_info_path (str): path/to/library_info.json
-        all_idotp_inputs (list of strings): list of all input IsotopeCluster-list filepaths
+        all_idotp_inputs (list of strings): list of all input idotp json filepaths
+        all_ics_inputs (list of strings): list of all input IsotopeCluster-list filepaths
         indices_out_path (str): path/to/filter_passing_indices.csv
         library_info_out_path (str): path/to/checked_library_info.json
         plot_out_path (str): path/to/file.png for idotp_distribution plot
@@ -63,25 +188,34 @@ def main(library_info_path,
 
     """
     library_info = pd.read_json(library_info_path)
-    out_df = pd.DataFrame(columns=list(library_info.columns)+['idotp', 'integrated_mz_width', 'mz_centers', 'theor_mz_dist'])
-    idotps = []
 
-    # Opens each idotp_check dataframe, if idotp>=cutoff adds computed values to row and appends row to output.
-    for idpc in all_idotp_inputs:
-        open_idpc = pd.read_json(idpc)
-        idotps.append(open_idpc["idotp"].values[0])
-        if open_idpc["idotp"].values>=0.99:
-            my_name = idpc.split("/")[-2]
-            my_charge = int([item[6:] for item in idpc.split("/")[-1].split("_") if "charge" in item][0])
-            my_row = library_info.loc[(library_info["name"]==my_name) & (library_info["charge"]==my_charge)]
-            for column in open_idpc.columns:
-                my_row[column] = open_idpc[column].values
-            out_df = out_df.append(my_row)
+    df = generate_dataframe_ics(configfile=configfile,
+                                all_ics_inputs=all_ics_inputs,
+                                idotp_cutoff=idotp_cutoff)
+
+    cols_idotp = ['idotp', 'integrated_mz_width', 'mz_centers', 'theor_mz_dist']
+    cols_ics_recenter = ['RT_weighted_avg', 'DT_weighted_avg_bins']
+
+    out_df = pd.DataFrame(columns=list(library_info.columns) + cols_idotp + cols_ics_recenter + ['name_recentered'])
+
+    for i, (name, charge) in enumerate(set([(i, j) for (i, j) in df[['name', 'charge']].values])):
+        open_idotp_f = pd.read_json([i for i in all_idotp_inputs if '%s' % (name + '_charge' + str(charge)) in i][0])
+        my_row = library_info.loc[(library_info["name"] == name) & (library_info["charge"] == charge)].copy()
+        my_row[cols_idotp] = open_idotp_f[cols_idotp].values
+        my_row[cols_corr] = \
+        df[(df['name'] == name) & (df['charge'] == charge)].sort_values(by='idotp', ascending=False)[
+            cols_ics_recenter].values[0]
+        my_row['name_recentered'] = '_'.join(name.split('_')[:-1]) + '_' + str(
+            round(my_row['RT_weighted_avg'].values[0], 3))
+        out_df = out_df.append(my_row)
 
     if library_info_out_path is not None:
         out_df.to_json(library_info_out_path)
 
     if plot_out_path is not None:
+        idotps = []
+        for f in all_idotp_inputs:
+            idotps.append(pd.read_json(f)['idotp'].values[0])
         sns.displot(idotps)
         plt.axvline(idotp_cutoff, 0, 1)
         plt.savefig(plot_out_path)
@@ -93,15 +227,20 @@ def main(library_info_path,
 if __name__ == "__main__":
     # If the snakemake global object is present, save expected arguments from snakemake to be passed to main().
     if "snakemake" in globals():
-        library_info_path = snakemake.input.pop(0)
-        all_idotp_inputs = snakemake.input
+        configfile_path = snakemake.input[0]
+        library_info_path = snakemake.input[1]
+        all_idotp_inputs = [item for item in snakemake.input if item.endswith("idotp_check.json")]
+        all_ics_inputs = [item for item in snakemake.input if item.endswith(".cpickle.zlib")]
 
-        library_info_out_path =  snakemake.output[0]
+        library_info_out_path = snakemake.output[0]
         plot_out_path = snakemake.output[1]
 
+        configfile = yaml.load(open(configfile_path, 'rb'), Loader=yaml.Loader)
 
-        main(library_info_path=library_info_path,
+        main(configfile=configfile,
+             library_info_path=library_info_path,
              all_idotp_inputs=all_idotp_inputs,
+             all_ics_inputs=all_ics_inputs,
              library_info_out_path=library_info_out_path,
              plot_out_path=plot_out_path)
     else:
@@ -110,10 +249,14 @@ if __name__ == "__main__":
             description=
             "Reads all rt-group idotp csvs and returns or saves a list of indices with idotp >= idotp_cutoff."
         )
+        parser.add_argument("configfile_path", help="path/to/config.conf")
         parser.add_argument("library_info_path", help="path/to/library_info.json")
         parser.add_argument("-i",
                             "--all_idotp_inputs",
                             help="list of all idotp check .json outputs to be read")
+        parser.add_argument("-a",
+                            "--all_ics_inputs",
+                            help="list of all ics cpickle.zlib outputs to be read")
         parser.add_argument("-d",
                             "--input_dir_path",
                             help="path/to/dir/ containing idotp_check.csv files")
@@ -143,8 +286,12 @@ if __name__ == "__main__":
 
         all_idotp_inputs = args.all_idotp_inputs.split(' ')
 
-        main(library_info_path=args.library_info_path,
+        configfile = yaml.load(open(args.configfile_path, 'rb'), Loader=yaml.Loader)
+
+        main(configfile=args.configfile,
+             library_info_path=args.library_info_path,
              all_idotp_inputs=args.all_idotp_inputs,
+             all_ics_inputs=args.all_ics_inputs,
              library_info_out_path = args.library_info_out_path,
              plot_out_path=args.plot_out_path,
              idotp_cutoff=args.idotp_cutoff)
