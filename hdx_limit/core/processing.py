e327f23aab08a7e7e399e6fb86aa6dfabfe0d140
Allan Ferrari <ajferrari@gmail.com>
2022-05-23 13:45:45 -0500

    v0.22.142 max_peak_center = len(seq) - 2 - count profiles from position 2 + undeuterated center of mass Remove ics with center of mass greater than max_peak_center

--- hdx_limit/core/processing.py
+++ hdx_limit/core/processing.py
@@ -422,18 +422,21 @@ class PathOptimizer:
         self.library_info = library_info
         self.timepoints = timepoints
         self.n_undeut_runs = n_undeut_runs
+        self.first_center = [ic for ic in all_tp_clusters[0] if ic.idotp > self.thresholds['idotp_cutoff']][0].baseline_integrated_mz_com
+
+
         if use_rtdt_recenter:
             self.max_peak_center = len(
                 self.library_info.loc[self.library_info["name_recentered"] ==
                                       self.name]["sequence"].values[0]
             ) - self.library_info.loc[self.library_info["name_recentered"] ==
-                                      self.name]["sequence"].values[0][2:].count('P') - 2
+                                      self.name]["sequence"].values[0][2:].count('P') - 2 + self.first_center
         else:
             self.max_peak_center = len(
                 self.library_info.loc[self.library_info["name"] ==
                                       self.name]["sequence"].values[0]
             ) - self.library_info.loc[self.library_info["name"] ==
-                                      self.name]["sequence"].values[0][2:].count('P') - 2
+                                      self.name]["sequence"].values[0][2:].count('P') - 2 + self.first_center
 
         self.old_data_dir = old_data_dir
         self.old_files = None
@@ -482,8 +485,7 @@ class PathOptimizer:
                                   ic.baseline_integrated_mz_FWHM >= self.thresholds[
                                       'baseline_integrated_FWHM'] and
                                   ic.nearest_neighbor_correlation >= self.thresholds['nearest_neighbor_correlation']
-                                  and ic.baseline_integrated_mz_com <= self.max_peak_center
-                                  + undeut_list[0].baseline_integrated_mz_com)
+                                  and ic.baseline_integrated_mz_com <= self.max_peak_center)
              ] for ics in self.prefiltered_ics[1:] if ics[0].timepoint_idx in self.timepoints]
         filtered_atc = np.array([undeut_list] + filtered_atc)
         filtered_indexes = np.array([True if len(ics) > 0 else False for ics in filtered_atc])
