d59c041b9bde2f89ebad66b7b651638fa4e822e7
Allan Ferrari <ajferrari@gmail.com>
2023-04-17 22:56:29 -0500

    v0.23.04.17
    Update PO to handle two stages

--- hdx_limit/pipeline/11_optimize_paths.py
+++ hdx_limit/pipeline/11_optimize_paths.py
@@ -274,27 +274,48 @@ if __name__ == "__main__":
         library_info_path = snakemake.input[0]
         configfile = yaml.load(open(snakemake.input[1], "rb").read(), Loader=yaml.Loader)
         all_timepoints_clusters_input_path = snakemake.input[2]
-        dictionary_thresholds_path = snakemake.input[3]
-        old_data_dir = None
+
         rt_group_name = snakemake.params.rt_group_name
-        prefiltered_ics_out_path = snakemake.output[0]
-        mono_path_plot_out_path = snakemake.output[1]
+        old_data_dir = None
         mono_html_plot_out_path = None
-        mono_winner_out_path = snakemake.output[2]
-        mono_runner_out_path = snakemake.output[3]
-        mono_undeut_ground_out_path = snakemake.output[4]
-        mono_winner_scores_out_path = snakemake.output[5]
-        mono_rtdt_com_cvs_out_path = snakemake.output[6]
-        mono_winner_csv_out_path = snakemake.output[7]
-        multi_path_plot_out_path = snakemake.output[8]
         multi_html_plot_out_path = None
-        multi_winner_out_path = snakemake.output[9]
-        multi_runner_out_path = snakemake.output[10]
-        multi_undeut_ground_out_path = snakemake.output[11]
-        multi_winner_scores_out_path = snakemake.output[12]
-        multi_rtdt_com_cvs_out_path = snakemake.output[13]
-        multi_winner_csv_out_path = snakemake.output[14]
         ajf_plot_out_path = None
+
+        if snakemake.params.tmp:
+            multi_winner_out_path = snakemake.output[0]
+            dictionary_thresholds_path = None
+            prefiltered_ics_out_path = None
+            mono_path_plot_out_path = None
+            mono_winner_out_path = None
+            mono_runner_out_path = None
+            mono_undeut_ground_out_path = None
+            mono_winner_scores_out_path = None
+            mono_rtdt_com_cvs_out_path = None
+            mono_winner_csv_out_path = None
+            multi_path_plot_out_path = None
+            multi_runner_out_path = None
+            multi_undeut_ground_out_path = None
+            multi_winner_scores_out_path = None
+            multi_rtdt_com_cvs_out_path = None
+            multi_winner_csv_out_path = None
+        else:
+            dictionary_thresholds_path = snakemake.input[3]
+            prefiltered_ics_out_path = snakemake.output[0]
+            mono_path_plot_out_path = snakemake.output[1]
+            mono_winner_out_path = snakemake.output[2]
+            mono_runner_out_path = snakemake.output[3]
+            mono_undeut_ground_out_path = snakemake.output[4]
+            mono_winner_scores_out_path = snakemake.output[5]
+            mono_rtdt_com_cvs_out_path = snakemake.output[6]
+            mono_winner_csv_out_path = snakemake.output[7]
+            multi_path_plot_out_path = snakemake.output[8]
+            multi_winner_out_path = snakemake.output[9]
+            multi_runner_out_path = snakemake.output[10]
+            multi_undeut_ground_out_path = snakemake.output[11]
+            multi_winner_scores_out_path = snakemake.output[12]
+            multi_rtdt_com_cvs_out_path = snakemake.output[13]
+            multi_winner_csv_out_path = snakemake.output[14]
+
 
 
         main(library_info_path=library_info_path,
